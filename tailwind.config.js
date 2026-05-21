2d056171c0a36eff0a04970fe4044873fc714aff
feliippeee <felipe.tecnosuporte@gmail.com>
2026-05-21 02:55:57 -0300

    Instalação do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [{
+   tailwindcss: {},
+    autoprefixer: {},
+}],
+}
+
+//content: [
+//  "./src/**/*.{html,ts}",
+//]
