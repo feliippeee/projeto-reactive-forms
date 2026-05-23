51b683702c34fa8294da3babc502c235205aca2a
feliippeee <felipe.tecnosuporte@gmail.com>
2026-05-22 21:55:07 -0300

    Criação das Interfaces e Tipagens do Endpoint de País

--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, Observable } from "rxjs";
+import { ICountriesResponse } from "../interfaces/countries-response/countries-response.interface";
+import { CountriesList } from "../types/countries-list";
 
 @Injectable({
     providedIn: "root",
@@ -11,8 +13,8 @@ export class CountriesService {
         private readonly _httpClient: HttpClient
     ) {}
 
-    getCountries(): any {
-        return this._httpClient.get<any>('https://countriesnow.space/api/v0.1/countries/positions').pipe(
+    getCountries(): Observable<CountriesList> {
+        return this._httpClient.get<ICountriesResponse>('https://countriesnow.space/api/v0.1/countries/positions').pipe(
             map((countriesResponse)=> {
                 return countriesResponse.data;
             })
