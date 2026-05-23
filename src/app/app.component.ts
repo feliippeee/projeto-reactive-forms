fff6c67996d9c874a91eecb53c5b9432bf47f5e6
feliippeee <felipe.tecnosuporte@gmail.com>
2026-05-23 02:16:15 -0300

    Criação do Service de Usuários

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CountriesService } from './services/countries.service';
 import { StateService } from './services/state.service';
 import { CitiesService } from './services/cities.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ export class AppComponent {
   constructor(
     private readonly _countriesService: CountriesService,
     private readonly _stateService: StateService,
-    private readonly _citiesService: CitiesService
+    private readonly _citiesService: CitiesService,
+    private readonly _usersService: UsersService,
   ) {}
 
   ngOnInit() {
@@ -26,6 +28,10 @@ export class AppComponent {
 
     this._citiesService.getCities('Brazil', 'São Paulo').subscribe((citiesResponse) => {
       console.log('citiesResponse', citiesResponse);
+    });
+
+    this._usersService.getUsers().subscribe((usersListResponse) => {
+      console.log('usersListResponse', usersListResponse);
     });
   }
 }
