dad5439d78daf6c71b54539eb6abe1bbda6b8c41
feliippeee <felipe.tecnosuporte@gmail.com>
2026-05-25 01:31:16 -0300

    Passagem da Lista de Usuários para o Componente via @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from './services/countries.service';
 import { StateService } from './services/state.service';
 import { CitiesService } from './services/cities.service';
 import { UsersService } from './services/users.service';
+import { UsersListResponse } from './types/users-list-response';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  usersList: UsersListResponse = [];
+  
   constructor(
     private readonly _countriesService: CountriesService,
     private readonly _stateService: StateService,
@@ -18,20 +22,18 @@ export class AppComponent {
   ) {}
 
   ngOnInit() {
-    this._countriesService.getCountries().subscribe((countriesResponse: any) => {
-      console.log('countriesResponse', countriesResponse);
-    });
+   // this._countriesService.getCountries().subscribe((countriesResponse: any) => {
+     // console.log('countriesResponse', countriesResponse);
+    //});
 
-    this._stateService.getStates('Brazil').subscribe((stateResponse) => {
-      console.log('stateResponse', stateResponse);
-    });
+    //this._stateService.getStates('Brazil').subscribe((stateResponse) => {
+      //console.log('stateResponse', stateResponse);
+    //});
 
-    this._citiesService.getCities('Brazil', 'São Paulo').subscribe((citiesResponse) => {
-      console.log('citiesResponse', citiesResponse);
-    });
+    //this._citiesService.getCities('Brazil', 'São Paulo').subscribe((citiesResponse) => {
+      //console.log('citiesResponse', citiesResponse);
+    //});
 
-    this._usersService.getUsers().subscribe((usersListResponse) => {
-      console.log('usersListResponse', usersListResponse);
-    });
+    this._usersService.getUsers().pipe(take(1)).subscribe((usersListResponse) => this.usersList = usersListResponse);
   }
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../angular-material/angular.material.module";
 import { PipesModule } from "../Pipes/pipes.module";
 import { UsersListComponent } from './users-list/users-list.component';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     declarations: [
     UsersListComponent
   ],
     imports: [
+        CommonModule, // Importa o CommonModule para usar diretivas comuns do Angular, usar o *ngFor, *ngIf, etc no user-list.html .
         AngularMaterialModule, // Importa o módulo AngularMaterialModule para usar os componentes do Angular Material
         PipesModule
     ],
