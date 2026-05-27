4ab533ed702c5c39bf6fec1b6516b0ec4bf83383
feliippeee <felipe.tecnosuporte@gmail.com>
2026-05-27 02:01:16 -0300

    Utilização do CurrencyPipe na Renda Mensal do Usuário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { AngularMaterialModule } from './angular-material/angular.material.modul
 import { ComponentsModule } from './components/components.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt); // cofiguração do pipe de moeda para o padrão brasileiro
+
 @NgModule({
   declarations: [
     AppComponent
