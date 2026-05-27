c693e068a65ba9429d897e6f1f8d504556f6fade
feliippeee <felipe.tecnosuporte@gmail.com>
2026-05-27 00:52:47 -0300

    Criação do Pipe de Estado Civil

---
+++ src/app/Pipes/marital-status.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { MaritalStatusEnum } from '../enums/matirial-status.enum';
+
+@Pipe({
+  name: 'maritalStatus'
+})
+export class MaritalStatusPipe implements PipeTransform {
+
+  transform(maritalStatus: number | undefined): string {
+    const maritalStatusMap: { [key: number]: string} = {
+      [MaritalStatusEnum.SINGLE]: 'Solteiro',
+      [MaritalStatusEnum.MARRIED]: 'Casado',
+      [MaritalStatusEnum.DIVORCED]: 'Divorciado',
+  };
+  return maritalStatus ? maritalStatusMap[maritalStatus] : ''; 
+}
+
+}
