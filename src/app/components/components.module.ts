8c054828189149b86497a052262a0f65fd4e6f5e
feliippeee <felipe.tecnosuporte@gmail.com>
2026-06-28 02:12:39 -0200

    Criação do Componente de Lista de Dependentes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -8,6 +8,7 @@ import { UserInfoItemComponent } from './user-info-item/user-info-item.component
 import { ContactInformationsComponent } from './contact-informations/contact-informations.component';
 import { PhoneListComponent } from './contact-informations/components/phone-list/phone-list.component';
 import { AddressListComponent } from './contact-informations/components/address-list/address-list.component';
+import { DependentsListComponent } from './dependents-list/dependents-list.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { AddressListComponent } from './contact-informations/components/address-
     UserInfoItemComponent,
     ContactInformationsComponent,
     PhoneListComponent,
-    AddressListComponent
+    AddressListComponent,
+    DependentsListComponent
   ],
     imports: [
         CommonModule, // Importa o CommonModule para usar diretivas comuns do Angular, usar o *ngFor, *ngIf, etc no user-list.html .
@@ -26,7 +28,8 @@ import { AddressListComponent } from './contact-informations/components/address-
     exports: [
         UsersListComponent,
         GeneralInformationsComponent,
-        ContactInformationsComponent
+        ContactInformationsComponent,
+        DependentsListComponent,
     ],
 })
 export class ComponentsModule {}
