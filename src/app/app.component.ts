e4499e20036e236e51379c73fb611d53c54bf09a
feliippeee <felipe.tecnosuporte@gmail.com>
2026-07-01 23:47:07 -0200

    Criação da Lógica de Seleção do Usuário: Parte 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,35 +5,49 @@ import { CitiesService } from './services/cities.service';
 import { UsersService } from './services/users.service';
 import { UsersListResponse } from './types/users-list-response';
 import { take } from 'rxjs';
+import { IUser } from './interfaces/user/user.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+
+export class AppComponent implements OnInit {
+  userSelectedIndex: number | undefined;
+  userSelected: IUser = {} as IUser; // criando um clone do usuário selecionado
   usersList: UsersListResponse = [];
-  currentTabIndex: number = 2;
+  currentTabIndex: number = 0;
+
   constructor(
     private readonly _countriesService: CountriesService,
     private readonly _stateService: StateService,
     private readonly _citiesService: CitiesService,
     private readonly _usersService: UsersService,
-  ) {}
+  ) { }
 
   ngOnInit() {
-   // this._countriesService.getCountries().subscribe((countriesResponse: any) => {
-     // console.log('countriesResponse', countriesResponse);
+    // this._countriesService.getCountries().subscribe((countriesResponse: any) => {
+    // console.log('countriesResponse', countriesResponse);
     //});
 
     //this._stateService.getStates('Brazil').subscribe((stateResponse) => {
-      //console.log('stateResponse', stateResponse);
+    //console.log('stateResponse', stateResponse);
     //});
 
     //this._citiesService.getCities('Brazil', 'São Paulo').subscribe((citiesResponse) => {
-      //console.log('citiesResponse', citiesResponse);
+    //console.log('citiesResponse', citiesResponse);
     //});
 
     this._usersService.getUsers().pipe(take(1)).subscribe((usersListResponse) => this.usersList = usersListResponse);
+  }
+  onUserSelected(userIndex: number) {
+    const userFound = this.usersList[userIndex];
+
+    if (userFound) {
+      this.userSelectedIndex = userIndex;
+      this.userSelected = structuredClone(userFound); // criando um clone do usuário selecionado
+      this.currentTabIndex = 0; // resetando o índice da aba para a primeira aba (Geral) ao selecionar um novo usuário
+    }
   }
 }
