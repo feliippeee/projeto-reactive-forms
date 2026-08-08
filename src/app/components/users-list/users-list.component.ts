4beaf16a5b0150cf2941ffcb74475a8fac26a3b3
feliippeee <felipe.tecnosuporte@gmail.com>
2026-08-08 21:06:01 -0200

    Criação da Lógica para Desabilitar os Usuários (no modo edição)

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -8,11 +8,14 @@ import { UsersListResponse } from '../../types/users-list-response';
 })
 export class UsersListComponent {
   userSelectedIndex: number | undefined;
-  @Input({ required: true }) usersList: UsersListResponse = [];
+  @Input({ required: true }) usersList: UsersListResponse = []; // Propriedade de entrada para receber a lista de usuários do componente pai
+  @Input({ required: true }) isInEditMode: boolean = false; // Propriedade de entrada para receber o estado de edição do componente pai
 
   @Output('OnUserSelected') onUserSelectedEmitt = new EventEmitter<number>();
 
   onUserSelected(userindex: number) {
+    if(this.isInEditMode) return; // Se estiver em modo de edição, não permitir a seleção de outro usuário
+    
     this.userSelectedIndex = userindex;
     this.onUserSelectedEmitt.emit(userindex);
   }
