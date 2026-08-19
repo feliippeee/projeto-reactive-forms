d277982eb9ac882d4dc0d10e7b6947fa920cab0e
feliippeee <felipe.tecnosuporte@gmail.com>
2026-08-19 01:34:42 -0300

    Criação da Função de Conversão do UserForm para User: Parte 2

--- src/app/utils/convert-user-form-to-user.ts
+++ src/app/utils/convert-user-form-to-user.ts
@@ -1,10 +1,12 @@
-import { IUserForm, IUserFormGeneralInformations } from "../interfaces/user-form.interface";
+import { IUserForm, IUserFormGeneralInformations, IUserFormPhone } from "../interfaces/user-form.interface";
 import { IUser } from "../interfaces/user/user.interface";
+import { PhoneList } from "../types/phone-list";
 
 export const convertUserFormToUser = (userForm: IUserForm): IUser => {
     let newUser: Partial<IUser> = {} as IUser;
 
     newUser = { ...convertGeneralInformations(userForm.generalInformations) };
+    newUser.phoneList = [ ...convertPhoneList(userForm.contactInformations.phoneList) ];
 
     return newUser as IUser;
 };
@@ -19,4 +21,16 @@ const convertGeneralInformations = (generalInformations: IUserFormGeneralInforma
         monthlyIncome: generalInformations.monthlyIncome,
         birthDate: '' // Implementar a lógica para converter a data de nascimento do formato do formulário para o formato do objeto IUser,
     };
+
 };
+
+ const convertPhoneList = (phoneList: IUserFormPhone[]): PhoneList => {
+        const newUserPhoneList: PhoneList = phoneList.map((phone) => ({
+            type: phone.type,
+            internationalCode: phone.number.substring(0, 2), // Implementar a lógica para extrair o código internacional do número de telefone do formulário
+            areaCode: phone.number.substring(2, 4), // Implementar a lógica para extrair o código de área do número de telefone do formulário
+            number: phone.number.substring(4), // Implementar a lógica para extrair o número de telefone do formulário
+        }));
+        return newUserPhoneList;
+    };
+             
