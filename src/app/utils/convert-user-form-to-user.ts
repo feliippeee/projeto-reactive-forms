375e7d71921313aab3a1a4e7f9e096c30b302ef1
feliippeee <felipe.tecnosuporte@gmail.com>
2026-08-19 01:51:47 -0300

    Criação da Função de Conversão do UserForm para User: Parte 4

--- src/app/utils/convert-user-form-to-user.ts
+++ src/app/utils/convert-user-form-to-user.ts
@@ -1,6 +1,7 @@
-import { IUserForm, IUserFormAddress, IUserFormGeneralInformations, IUserFormPhone } from "../interfaces/user-form.interface";
+import { IUserForm, IUserFormAddress, IUserFormDependent, IUserFormGeneralInformations, IUserFormPhone } from "../interfaces/user-form.interface";
 import { IUser } from "../interfaces/user/user.interface";
 import { AddressList } from "../types/address-list";
+import { DependentsList } from "../types/dependents-list";
 import { PhoneList } from "../types/phone-list";
 
 export const convertUserFormToUser = (userForm: IUserForm): IUser => {
@@ -9,7 +10,8 @@ export const convertUserFormToUser = (userForm: IUserForm): IUser => {
     newUser = { ...convertGeneralInformations(userForm.generalInformations) };
     newUser.phoneList = [ ...convertPhoneList(userForm.contactInformations.phoneList) ];
     newUser.addressList = [ ...convertAddressList(userForm.contactInformations.addressList) ];
-
+    newUser.dependentsList = [ ...convertDependentsList(userForm.dependentsList) ];
+    
     return newUser as IUser;
 };
 
@@ -46,5 +48,13 @@ const convertAddressList = (addressList: IUserFormAddress[]): AddressList => {
         city: address.city,
     }));
     return newUserAddressList;
-}
-             
+};
+     
+const convertDependentsList = (dependentsList: IUserFormDependent[]): DependentsList => {
+    const newUserDependentsList: DependentsList = dependentsList.map((dependent) => ({
+        name: dependent.name,
+        age: Number(dependent.age),
+        document: Number(dependent.document),
+    }));
+    return newUserDependentsList;
+};
